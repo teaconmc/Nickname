28c0e57d325530db0754bfe891208a7fc20fe6d1
3TUSK <urey.s.knowledge@gmail.com>
2020-07-04 13:05:20 -0700

    Trick Minecraft by using a constructed packet
    We leverage the SPacketListItemPacket to sync the display name.
    Previously, we rely on func_175396_E (getTabListDisplayName) to get the
    current nick. Now, we just construct the packet with the right data.
    This should improve compatibilities with other mods.

--- src/main/java/org/teacon/nickname/NicknameCommand.java
+++ src/main/java/org/teacon/nickname/NicknameCommand.java
@@ -1,10 +1,16 @@
 package org.teacon.nickname;
 
+import java.util.Collections;
+import java.util.List;
+
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import it.unimi.dsi.fastutil.objects.ObjectArrays;
 import net.minecraft.command.CommandSource;
@@ -12,8 +18,11 @@ import net.minecraft.command.Commands;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.play.server.SPlayerListItemPacket;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 public final class NicknameCommand {
+
+    private static final Logger LOGGER = LogManager.getLogger("Nickname");
 
     public NicknameCommand(CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(Commands.literal("nick").then(
@@ -29,8 +38,7 @@ public final class NicknameCommand {
             String previous = NicknameMod.NICKS.put(player.getGameProfile().getId(), current);
             context.getSource().sendFeedback(previous == null ? new TranslationTextComponent("commands.nickname.nickname.set", current)
                 : new TranslationTextComponent("commands.nickname.nickname.changed", previous, current), true);
-            context.getSource().getServer().getPlayerList()
-                .sendPacketToAllPlayers(new SPlayerListItemPacket(SPlayerListItemPacket.Action.UPDATE_DISPLAY_NAME, player));
+            context.getSource().getServer().getPlayerList().sendPacketToAllPlayers(constructPacketFor(player));
             return Command.SINGLE_SUCCESS;
         } catch (CommandSyntaxException e) {
             context.getSource().sendErrorMessage(new TranslationTextComponent("commands.nickname.nickname.error", ObjectArrays.EMPTY_ARRAY));
@@ -43,13 +51,24 @@ public final class NicknameCommand {
             ServerPlayerEntity player = context.getSource().asPlayer();
             NicknameMod.NICKS.remove(player.getGameProfile().getId());
             context.getSource().sendFeedback(new TranslationTextComponent("commands.nickname.nickname.cleared", ObjectArrays.EMPTY_ARRAY), true);
-            context.getSource().getServer().getPlayerList()
-                .sendPacketToAllPlayers(new SPlayerListItemPacket(SPlayerListItemPacket.Action.UPDATE_DISPLAY_NAME, player));
+            context.getSource().getServer().getPlayerList().sendPacketToAllPlayers(constructPacketFor(player));
             return Command.SINGLE_SUCCESS;
         } catch (CommandSyntaxException e) {
             context.getSource().sendErrorMessage(new TranslationTextComponent("commands.nickname.nickname.error", ObjectArrays.EMPTY_ARRAY));
             return -1;
         }
+    }
+
+    private static SPlayerListItemPacket constructPacketFor(ServerPlayerEntity player) {
+        final SPlayerListItemPacket packet = new SPlayerListItemPacket(SPlayerListItemPacket.Action.UPDATE_DISPLAY_NAME, Collections.emptyList());
+        try {
+            List<SPlayerListItemPacket.AddPlayerData> playerData = ObfuscationReflectionHelper.getPrivateValue(SPlayerListItemPacket.class, packet, "field_179769_b");
+            playerData.add(packet.new AddPlayerData(player.getGameProfile(), player.ping, player.interactionManager.getGameType(), player.getDisplayName()));
+        } catch (Exception e) {
+            LOGGER.warn("Failed to construct PlayerListItemPacket, nickname will be out of sync. Check debug.log for more information.");
+            LOGGER.debug("Details: {}", e);
+        }
+        return packet;
     }
 
 }
--- src/main/resources/hooks.js
+++ src/main/resources/hooks.js
@@ -1,31 +1,5 @@
 function initializeCoreMod() {
     return {
-        "tab_display_name": {
-            "target": {
-                "type": "METHOD",
-                "class": "net.minecraft.entity.player.ServerPlayerEntity",
-                "methodName": "func_175396_E", // getTabListDisplayName
-                "methodDesc": "()Lnet/minecraft/util/text/ITextComponent;"
-            },
-            "transformer": function (method) {
-                var ASMAPI = Java.type('net.minecraftforge.coremod.api.ASMAPI');
-                var Opcodes = Java.type('org.objectweb.asm.Opcodes');
-                var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
-                var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
-
-                method.instructions.clear();
-                method.instructions.add(new VarInsnNode(Opcodes.ALOAD, 0));
-                method.instructions.add(ASMAPI.buildMethodCall(
-                    "net/minecraft/entity/player/PlayerEntity",
-                    ASMAPI.mapMethod("func_145748_c_"), // getDisplayName
-                    "()Lnet/minecraft/util/text/ITextComponent;",
-                    ASMAPI.MethodType.VIRTUAL
-                ));
-                method.instructions.add(new InsnNode(Opcodes.ARETURN));
-
-                return method;
-            }
-        },
         "get_display_name": {
             "target": {
                 "type": "METHOD",
